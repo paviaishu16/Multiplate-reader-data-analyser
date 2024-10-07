cacd0544234472dfd4dd782ccc5f29f4b52154d7
Simon <gevhaz@tutanota.com>
2024-10-08 01:30:20 +0900

    Apply smoothing to MTP data
    Added a function for applying LOESS smoothing to data and apply it for
    the user-supplied data.

--- tests/test_noise_removal.py
+++ tests/test_noise_removal.py
@@ -3,7 +3,7 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
-from noise_removal import normalize
+from noise_removal import apply_loess_smoothing, normalize
 
 
 def test_normalize():
@@ -22,3 +22,23 @@ def test_normalize():
     )
     actual_normalized_data = normalize(input_data)
     assert_frame_equal(actual_normalized_data, expected_normalized_data)
+
+
+def test_loess_smoothing():
+    """Test that values are smoothed as expected."""
+    input_data = pd.DataFrame(
+        {
+            "Time": [0.5, 1.0, 1.5, 2.0, 2.5],
+            "A1": [5.0, 10.0, 100.0, 100.0, 200.0],
+            "A2": [5.0, 10.0, 100.0, 100.0, 400.0],
+        }
+    )
+    expected_smoothed_data = pd.DataFrame(
+        {
+            "Time": [0.5, 1.0, 1.5, 2.0, 2.5],
+            "A1": [5.0, 6.0, 28.0, 100.0, 120.0],
+            "A2": [5.0, 6.0, 28.0, 100.0, 160.0],
+        }
+    )
+    actual_smoothed_data = apply_loess_smoothing(input_data)
+    assert_frame_equal(actual_smoothed_data, expected_smoothed_data)
