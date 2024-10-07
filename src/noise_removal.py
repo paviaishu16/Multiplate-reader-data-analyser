dce228c26e59765f5f9d61ce8ddb37c8a4b21aac
Simon <gevhaz@tutanota.com>
2024-10-07 23:38:25 +0900

    Normalize data
    Add a normalization function for normalizing all well data so that all
    values are between 0 and 1 according to the following formula applied
    per column:
    
    Normalized Value = (Current Value - Min value)/(Max value - Min value)

---
+++ src/noise_removal.py
@@ -0,0 +1,16 @@
+"""Functions related to noise removal based on blank wells."""
+
+import pandas as pd
+
+
+def normalize(data: pd.DataFrame) -> pd.DataFrame:
+    """Normalize all value in dataframe except first column."""
+
+    def normalize_column(s: pd.Series) -> pd.Series:  # type: ignore
+        min_val = s.min()
+        max_val = s.max()
+        return (s - min_val) / (max_val - min_val)  # type: ignore
+
+    data.iloc[:, 1:] = data.iloc[:, 1:].apply(normalize_column)
+
+    return data
---
+++ tests/test_noise_removal.py
@@ -0,0 +1,24 @@
+"""Tests related to noise removal based on blank wells."""
+
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from noise_removal import normalize
+
+
+def test_normalize():
+    """Test that values are normalized as expected."""
+    input_data = pd.DataFrame(
+        {
+            "Time": [0.5, 1.0, 1.5, 2.0, 2.5],
+            "A1": [500.0, 750.0, 1000.0, 1000.0, 1000.0],
+        }
+    )
+    expected_normalized_data = pd.DataFrame(
+        {
+            "Time": [0.5, 1.0, 1.5, 2.0, 2.5],
+            "A1": [0.0, 0.5, 1.0, 1.0, 1.0],
+        }
+    )
+    actual_normalized_data = normalize(input_data)
+    assert_frame_equal(actual_normalized_data, expected_normalized_data)
